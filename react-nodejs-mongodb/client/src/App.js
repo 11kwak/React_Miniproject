fbb13af1c1e1b3d44600de4783bd09099dc10119
kwakeleven <87745990+kwakeleven@users.noreply.github.com>
2021-11-02 01:32:04 +0900

    update img

--- react-nodejs-mongodb/client/src/App.js
+++ react-nodejs-mongodb/client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Container } from 'react-bootstrap';
 import Layout from "./layouts/Layout";
 
+
 function App() {
   return (
     <Layout>
-      <Container style={{ minHeight: "75vh" }}>야야야</Container>
-    </Layout>
+      <Container style={{ minHeight: "75vh" }}><img width="800px" src={require("./img/portfolio.jpg").default} ></img></Container>
+    </Layout >
   );
 }
 
